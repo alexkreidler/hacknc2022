dc6a770019992a6b047488414ea07dc2be6c3b9c
carraya <arrayacj@gmail.com>
2022-11-06 00:02:01 -0400

    feat(broken): frontend logic for speech recording

--- frontend/components/CustomMessenger.tsx
+++ frontend/components/CustomMessenger.tsx
@@ -11,9 +11,94 @@ import {
     useColorModeValue,
     Switch,
     Textarea,
+    useBoolean
   } from '@chakra-ui/react';
+  import axios from 'axios';
+  import { ReactMic } from 'react-mic';
+  import React, { useState, useEffect, useRef } from 'react';
+
   
   export default function SocialProfileSimple() {
+    const [isText, setIsText] = useBoolean();
+    const [transcribedData, setTranscribedData] = useState([]);
+    const [interimTranscribedData, ] = useState('');
+    const [isRecording, setIsRecording] = useState(false);
+    const [isTranscribing, setIsTranscribing] = useState(false);
+    const [selectedLanguage, setSelectedLanguage] = useState('english');
+    const [selectedModel, setSelectedModel] = useState(1);
+    const [transcribeTimeout, setTranscribeTimout] = useState(5);
+    const [stopTranscriptionSession, setStopTranscriptionSession] = useState(false);  
+
+  const intervalRef = React.createRef();
+  
+  const stopTranscriptionSessionRef = useRef(stopTranscriptionSession);
+  stopTranscriptionSessionRef.current = stopTranscriptionSession;
+
+  const selectedLangRef = useRef(selectedLanguage);
+  selectedLangRef.current = selectedLanguage;
+
+  const selectedModelRef = useRef(selectedModel);
+  selectedModelRef.current = selectedModel;
+
+  const supportedLanguages = ['english', 'chinese', 'german', 'spanish', 'russian', 'korean', 'french', 'japanese', 'portuguese', 'turkish', 'polish', 'catalan', 'dutch', 'arabic', 'swedish', 'italian', 'indonesian', 'hindi', 'finnish', 'vietnamese', 'hebrew', 'ukrainian', 'greek', 'malay', 'czech', 'romanian', 'danish', 'hungarian', 'tamil', 'norwegian', 'thai', 'urdu', 'croatian', 'bulgarian', 'lithuanian', 'latin', 'maori', 'malayalam', 'welsh', 'slovak', 'telugu', 'persian', 'latvian', 'bengali', 'serbian', 'azerbaijani', 'slovenian', 'kannada', 'estonian', 'macedonian', 'breton', 'basque', 'icelandic', 'armenian', 'nepali', 'mongolian', 'bosnian', 'kazakh', 'albanian', 'swahili', 'galician', 'marathi', 'punjabi', 'sinhala', 'khmer', 'shona', 'yoruba', 'somali', 'afrikaans', 'occitan', 'georgian', 'belarusian', 'tajik', 'sindhi', 'gujarati', 'amharic', 'yiddish', 'lao', 'uzbek', 'faroese', 'haitian creole', 'pashto', 'turkmen', 'nynorsk', 'maltese', 'sanskrit', 'luxembourgish', 'myanmar', 'tibetan', 'tagalog', 'malagasy', 'assamese', 'tatar', 'hawaiian', 'lingala', 'hausa', 'bashkir', 'javanese', 'sundanese']
+
+  const modelOptions = ['tiny', 'base', 'small', 'medium', 'large']
+
+  useEffect(() => {
+    // return () => clearInterval(intervalRef.current);
+  }, []);
+
+
+  function handleTranscribeTimeoutChange(newTimeout) {
+    setTranscribeTimout(newTimeout)
+  }
+
+  function startRecording() {
+    setStopTranscriptionSession(false)
+    setIsRecording(true)
+    // intervalRef.current = setInterval(transcribeInterim, transcribeTimeout * 1000)
+  }
+
+  function stopRecording() {
+    // clearInterval(intervalRef.current);
+    setStopTranscriptionSession(true)
+    setIsRecording(false)
+    setIsTranscribing(false)
+  }
+
+  function onData(recordedBlob) {
+    // console.log('chunk of real-time data is: ', recordedBlob);
+  }
+
+  function onStop(recordedBlob) {
+    transcribeRecording(recordedBlob)
+    setIsTranscribing(true)  
+  }
+
+  function transcribeInterim() {
+    // clearInterval(intervalRef.current);
+    setIsRecording(false)
+  }
+  function transcribeRecording(recordedBlob) {
+    const headers = {
+      "content-type": "multipart/form-data",
+    };
+    const formData = new FormData();
+    formData.append("language", selectedLangRef.current)
+    formData.append("model_size", modelOptions[selectedModelRef.current])
+    formData.append("audio_data", recordedBlob.blob, 'temp_recording');
+    axios.post("http://0.0.0.0:9000/transcribe", formData, { headers })
+      .then((res) => {
+        setTranscribedData(oldData => [...oldData, res.data])
+        setIsTranscribing(false)
+        setIsRecording(false)
+        // intervalRef.current = setInterval(transcribeInterim, transcribeTimeout * 1000)
+      });
+      
+      // if (!stopTranscriptionSessionRef.current){
+      //   setIsRecording(true)
+      // }
+  }
     return (
       <Center py={6}>
         <Box
@@ -51,15 +136,33 @@ import {
           <Stack align={'center'} justify={'center'} direction={'row'} mt={6}>
             {/* put toggle switch for text/speech here */}
             <Text>Text</Text>
-            <Switch></Switch>
+            <Switch onChange={() => {
+              setIsText.toggle();
+              console.log(isText);
+            }}></Switch>
             <Text>Voice</Text>
-
+            
           </Stack>
-  
-          <Stack mt={8} direction={'row'} spacing={4}>
-            {/* put type / bullshit here */}
-            <Textarea></Textarea>
-          </Stack>
+          <div>
+            {!isText ? (
+              <Stack mt={8} direction={'row'} spacing={4}>
+              {/* put type / bullshit here */}
+              <Textarea></Textarea>
+            </Stack>
+            ) : (
+              <Stack>
+                <Button colorScheme="red" size="md">Record Voice</Button>
+                <div className="recordIllustration">
+                  <ReactMic record={isRecording} className="sound-wave" onStop={onStop}
+                  onData={onData} strokeColor="#0d6efd" backgroundColor="#f6f6ef" />
+                </div>
+                <div>
+                  <h1>{transcribedData}</h1>
+                </div>
+              </Stack>
+            )}
+          </div>
+          
         </Box>
       </Center>
     );
