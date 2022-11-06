19f49f020956e0f9fc0c93c09819a977275348fa
Alex Kreidler <contributions@alexkreidler.com>
2022-11-06 11:34:26 -0500

    add to one file

--- linguabot-chat-service/api.py
+++ linguabot-chat-service/api.py
@@ -1,12 +1,13 @@
 
 from dataclasses import dataclass
 from nanoid import generate
-from typing import Union
+from typing import Optional, Union
 
 from fastapi.encoders import jsonable_encoder
 from fastapi import FastAPI
 
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi import File, Form, UploadFile
 
 from pydantic import BaseModel
 app = FastAPI()
@@ -70,6 +71,44 @@ def send_message(chat_id: str, message: SimpleMessage):
     print(out[1])
     
     return jsonable_encoder(chat) #{"id": chat_id, "history": chat.history}
+
+
+
+from google.cloud import speech
+from google.cloud.speech import SpeechRecognitionResult
+@app.post("/transcribe")
+def transcribe_audio(audio_data: bytes = File(), language: Optional[str] = Form()):
+    # content = await audio_data.read()
+    client = speech.SpeechClient()
+
+    audio = speech.RecognitionAudio(content=audio_data)
+    config = speech.RecognitionConfig(
+        # encoding=speech.RecognitionConfig.AudioEncoding.LINEAR16,
+        # sample_rate_hertz=16000,
+        language_code=language,
+        # "en-US",
+    )
+
+    response = client.recognize(config=config, audio=audio)
+
+    # Each result is for a consecutive portion of the audio. Iterate through
+    # them to get the transcripts for the entire audio file.
+    transcript = ""
+    results: list[SpeechRecognitionResult] = response.results
+    
+    out = []
+    for result in results:
+        most_likely_chunk = result.alternatives[0].transcript
+        transcript += most_likely_chunk
+        
+        result_json = SpeechRecognitionResult.to_dict(result)
+        out.append(result_json)
+
+    print("Handling request, transcript length", len(transcript))
+    f = {"transcript": transcript, "details": out}
+    # print(f)
+    return f
+
 
 status = "ok"
 
